dcc6864f0de24cfbe022a786207f26aa45960da7
Stuzerain <shanford3@gmail.com>
2020-11-04 11:28:05 -0800

    Add rendering of 'eventbox' component for each returned result

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+import EventBox from './eventbox.jsx';
+
 const App = () => {
 
+  const [events, setEvents] = useState([]);
+
   const testFetch = () => {
-    axios.get('/events?date=-300')
+    axios.get('/events?_page=4&_limit=10')
       .then(results => {
-        console.log(results.data);
+        console.log(results);
+        setEvents(results.data)
       })
   }
+
+  let eventBoxes = events.map((event, index) =>
+    <EventBox event={event} key={index} />
+  )
 
   return (
     <div>
      <h1>Historical Event Finder</h1>
 
     <button onClick={() => testFetch()}>TEST</button>
+
+    {eventBoxes}
 
     </div>
   )
---
+++ challenge_1/client/components/eventbox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const EventBox = ({event}) => {
+
+  return (
+    <div>
+     <p>box</p>
+
+    </div>
+  )
+}
+ export default EventBox;
