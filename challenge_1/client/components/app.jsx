cf5d9c028656e365a30093bf11446a6db75f4ead
Stuzerain <shanford3@gmail.com>
2020-11-04 11:17:30 -0800

    Implement test axios request to json-server via button in client

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const App = () => {
+
+  const testFetch = () => {
+    axios.get('/events?date=-300')
+      .then(results => {
+        console.log(results.data);
+      })
+  }
 
   return (
     <div>
      <h1>Historical Event Finder</h1>
 
-
+    <button onClick={() => testFetch()}>TEST</button>
 
     </div>
   )
