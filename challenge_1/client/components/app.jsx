3dcd55f9071f1be7418714f5f491efac692d4de2
Stuzerain <shanford3@gmail.com>
2020-11-04 11:33:10 -0800

    Added input to keep track of keyword for searching

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -6,10 +6,15 @@ import EventBox from './eventbox.jsx';
 
 const App = () => {
 
+  const [keyword, setKeyword] = useState('')
   const [events, setEvents] = useState([]);
 
-  const testFetch = () => {
-    axios.get('/events?_page=4&_limit=10')
+  const handleFormInputChange = (event) => {
+    setKeyword(event.target.value);
+  }
+
+  const loadResults = () => {
+    axios.get('/events?_page=1&_limit=10')
       .then(results => {
         console.log(results);
         setEvents(results.data)
@@ -24,7 +29,8 @@ const App = () => {
     <div>
      <h1>Historical Event Finder</h1>
 
-    <button onClick={() => testFetch()}>TEST</button>
+    <input type='text' value={keyword} onChange={(e) => {handleFormInputChange(event)}}></input>
+    <button onClick={() => testFetch()}>Load results</button>
 
     {eventBoxes}
 
