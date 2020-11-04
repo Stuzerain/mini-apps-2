acde6f06543cab6f5d0b474c06c526ee42e20119
Stuzerain <shanford3@gmail.com>
2020-11-04 12:13:12 -0800

    Further modularize app.jsx by moving all searching to search.jsx

--- challenge_1/client/components/eventbox.jsx
+++ challenge_1/client/components/eventbox.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 
 const EventBox = ({event}) => {
 
+  let place = event.category2 ? <h4>Place: {event.category2}</h4> : '';
+
   return (
     <div style={{border: '1px solid black'}}>
      <h4>Date: {event.date}</h4>
-      <h4>Place: {event.category2}</h4>
+      {place}
       <p>{event.description}</p>
     </div>
   )
