b610520200dd1c5aa96468bfb07bfc959c34bfc1
Stuzerain <shanford3@gmail.com>
2020-11-04 11:57:57 -0800

    Cleanups, beginning to separate app, eventbox, search bar more

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -6,21 +6,26 @@ import EventBox from './eventbox.jsx';
 
 const App = () => {
 
+  // state that holds the keyword that will be used to search
   const [keyword, setKeyword] = useState('')
+  // state that holds the results of a search
   const [events, setEvents] = useState([]);
 
+  // function that keeps the 'keyword' state consistent with what is typed in the search box
   const handleFormInputChange = (event) => {
     setKeyword(event.target.value);
   }
 
-  const loadResults = () => {
-    axios.get('/events?_page=1&_limit=10')
+  // function that queries the json-server using the 'keyword' state -- it specifies that the results come in pages of 10 at a time
+  const loadResults = (search) => {
+    axios.get(`/events?_page=1&_limit=10&q=${search}`)
       .then(results => {
         console.log(results);
         setEvents(results.data)
       })
   }
 
+  // this will render one EventBox component for each event that comes back from the search (up to a max of 10)
   let eventBoxes = events.map((event, index) =>
     <EventBox event={event} key={index} />
   )
@@ -30,7 +35,7 @@ const App = () => {
      <h1>Historical Event Finder</h1>
 
     <input type='text' value={keyword} onChange={(e) => {handleFormInputChange(event)}}></input>
-    <button onClick={() => testFetch()}>Load results</button>
+    <button onClick={() => loadResults(keyword)}>Load results</button>
 
     {eventBoxes}
 
--- challenge_1/client/components/eventbox.jsx
+++ challenge_1/client/components/eventbox.jsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 const EventBox = ({event}) => {
 
   return (
-    <div>
-     <p>box</p>
-
+    <div style={{border: '1px solid black'}}>
+     <h4>Date: {event.date}</h4>
+      <h4>Place: {event.category2}</h4>
+      <p>{event.description}</p>
     </div>
   )
 }
