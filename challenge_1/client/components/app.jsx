527bd74f13ab044423a7afdf03dbcc6047197151
Stuzerain <shanford3@gmail.com>
2020-11-04 11:13:22 -0800

    Basic setup/dependency installation

---
+++ challenge_1/client/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+  return (
+    <div>
+     <h1>Historical Event Finder</h1>
+
+
+
+    </div>
+  )
+}
+ export default App;
